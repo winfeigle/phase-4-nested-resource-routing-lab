afb3c8472e34e335a58642c89de2117732161592
Win Feigle <win.feigle@gmail.com>
2023-01-04 07:41:49 -0700

    commit tree 056f846b90568ff19b78bf205f8954b86ed96419

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,35 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_error
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find_by!(id: params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    item = user.items.create(item_params)
+    render json: item, status: :created
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
+  def render_not_found_error
+    render json: { error: "Not found"}, status: :not_found
   end
 
 end
