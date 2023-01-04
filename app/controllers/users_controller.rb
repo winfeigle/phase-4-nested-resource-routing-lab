9f9535daa77dda98fac69b35f11d06e02b1e6e23
Win Feigle <win.feigle@gmail.com>
2023-01-04 07:54:00 -0700

    Complete Lab

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def show
-    user = User.find_by(id: params[:id])
+    user = User.find(params[:id])
     render json: user, include: :items
   end
 
